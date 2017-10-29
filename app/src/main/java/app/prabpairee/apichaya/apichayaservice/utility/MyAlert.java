5bfbc329e6e1c71093d171cbbed76671fb8093ec
poo <poo-19@hotmail.com>
2017-10-29 10:24:06 +0700

    Start on 29 Oct 2017

---
+++ app/src/main/java/app/prabpairee/apichaya/apichayaservice/utility/MyAlert.java
@@ -0,0 +1,18 @@
+package app.prabpairee.apichaya.apichayaservice.utility;
+
+import android.content.Context;
+
+/**
+ * Created by Thanapat on 29/10/2017.
+ */
+
+public class MyAlert {
+
+    //    Explicit
+    private Context context;
+
+//    การสร้าง Constructor โดย Alt + Insert
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+}   // Main Class
